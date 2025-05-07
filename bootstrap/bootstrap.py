0b7ddc40c50dc11073e4e23dad830ce8af532f6c
muneeb3878 <mu445611@gmail.com@gmail.com>
2025-05-07 21:14:31 +0530

    changes done

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -1,21 +1,10 @@
 
 # bootstrap
-# bootstrap ye bohot asaan kar deta he html css ko aur isme build in class hoti jiska use karke styling karna aur asaan ho jata he
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# bootstrap ye bohot asaan kar deta he html css ko aur isme build in class aur javascript hoti he 
+# jiska use karke styling karna aur asaan ho jata he
+# isme ek link tag hota he jisme class likhi hoti he 
+# aur isme script hota he jisme javascript likhi hoti he
+# agar iske components use karna he to inke url ko link me aur script me include karna padega 
 
 
 
