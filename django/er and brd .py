81fbdddbcf887b0d75eface9206186a023600298
muneeb3778 <murrehman377@gmail.com>
2025-05-20 23:24:58 +0530

    changes done

---
+++ django/er and brd .py
@@ -0,0 +1,96 @@
+
+# er model and er diagram
+# entity relational model
+# er diagram batata he jo database banaya he uska structure kesa he diagram ke form me aur er model ye batata he usme konsa table use kiya he
+ 
+# relationship in er model
+# relationship usko bolte jo table apas me connected he matlab jo table foreign key se connect hoti he usi ko relationship bolte he
+# aur er diagram ka relationalship 3 types ka hota he 1 to 1 ,1 to many, many to many
+
+# jab er-diagram banate he tab ham ye graphical symbols use karte he
+# rectangle for entity (table)
+# elip (oval) for attribute
+# diamont for relationship
+# elip me line for primary key
+# double rectangle for weak entity matlab esa table jisme zada records nahi hote
+# double diamont for weak relationship
+# double elips for multivalues attributes
+# elip--elip--elip for composite attibute 
+ 
+# rules design a er diagram  
+# indentify the active element matlab table among entity 
+# indentify all the relational among entity matlab jo entity ke andar entity he unke relation pata karna
+# find the key attribute of entity 
+# indentify the other attribute of entity matlab key attibute ko chodke jo attibutes he
+# draw the complete er-diagram
+# review the diagram to check all the relationship and attributes are drawn or not
+ 
+
+
+
+
+# how to use microsoft word
+# ye isliye use karte he diagram wagera banane ke liye isme bohot saari chize hoti jese text aline,font,colouring,design etc
+
+
+
+
+ 
+# requirment and requirement document
+ 
+# what is a requirement 
+# requirement matlab kisi cheez ki demand ya zarurat hona to ham kese sakte he functionalaties software ki vo requirement he
+
+# what is a requirement document
+# requirement document vo hota jo ham requirement ko document karke rakhte he
+
+# example of a requirement
+# jese koi ecommerce company he usme bas ek hi payment method laga he aur vo kharab hogaya to company ko usse bohot lose hoga to uske liye ek requirement document banana hoga jisme aur bhi payment karne ka option hoga
+
+
+# what is a business requirement document (brd)
+# business requirement document vo hota he jo ham kisi document file me business ke sare requirements ko rakte hertyuyt
+
+# example of a business requirement
+# ek bank he jisme saara kaam offline hota he jese account opening ke liye bhi bank jana hoga fund ko transfer karne ke liye bhi bank jana ho money withdrawal ke liye bhi bank jana ho etc 
+# to hame ek business requirement document banana hoga jisme business se related saare requirements honge jese online money send karna aur online money withdraw karna etc
+# aur jo business requirement hoti he vo what and why of a project
+
+# what is functional requirement document (frd)
+# functional requirement business requirement ka implementation hota he to jo business requirement hota he vo hota he what and why of a project aur functional requirement hota he how of a project
+
+# example of a functional requirement
+# jo business requirement ka example he usko implement karna jese online fund transfer karne ke liye paytm ya phonepay ka feature dalna aur 24/7 call center avalable karna etc
+
+# different between functional and business requirement
+#    business requirement      |     functional requirement
+# its what and why of project  | its how of project
+# isme jo requirement hote he  | isme jo requirement vo specify karte he features and functionalities
+# vo business point of view ke | to meet the business requirement
+# purpose se hoti he
+
+
+
+# why should we create a requirement document 
+# ye isliye zaruri hota he agar ham chate he ki project ko successfully development ya implementation of project karna he
+
+
+# ques = lets take a scenario for a retail store of grocery and the grocery store is becoming famous day by day and its very difficult for the owner of the store to handle
+# all the customer using a single store then what do you suggest,the retail store owner should do in order to increase its business
+# and what should be the business requirement and function requirement if the owner wants to opt for a new business model
+
+# business requirement document for this store
+# the owner should increase his store because customer dont need to wait
+# and work should be online to maximise his growth
+
+# function requirement document for this store
+# he can increase his store by developing more branches and maximise his income
+# he can make it online through ecommerce website  
+
+
+
+
+
+
+
+
